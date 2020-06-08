b8ffaffe18ba52961f2d9e067066544baaa04da6
Luuk van Houdt <luuk@vhoudt.nl>
2020-06-08 09:57:48 +0200

    Update .env
    WORDPRESS_WEBSITE_URL
    WORDPRESS_WEBSITE_URL_WITHOUT_HTTP
    
    Beide moeten leeg staan.

--- plugins/essif-lab/tests/Stubs/Utility.php
+++ plugins/essif-lab/tests/Stubs/Utility.php
@@ -7,7 +7,7 @@ use TNO\EssifLab\Utilities\Contracts\BaseUtility;
 use TNO\EssifLab\Utilities\WP;
 
 class Utility extends BaseUtility {
-	private $history = [];
+	use WithHistory;
 
 	static $meta = array();
 
@@ -19,16 +19,37 @@ class Utility extends BaseUtility {
 
 	protected $valueReturningFunctions = [
 		BaseUtility::GET_CURRENT_MODEL => [self::class, 'getCurrentModel'],
+<<<<<<< HEAD
         BaseUtility::GET_MODEL => [self::class, 'getModel'],
         BaseUtility::CREATE_MODEL_META => [self::class, 'createModelMeta'],
         BaseUtility::DELETE_MODEL_META => [self::class, 'deleteModelMeta'],
+=======
+		BaseUtility::GET_MODEL => [self::class, 'getModel'],
+>>>>>>> 6a9c1f7... Fixed broken tests after refactoring
 		BaseUtility::GET_MODEL_META => [self::class, 'getModelMeta'],
 		BaseUtility::GET_MODELS => [self::class, 'getModels'],
 	];
 
+<<<<<<< HEAD
     function call(string $name, ...$parameters) {
 		$wasCalled = count($this->getHistoryByFuncName($name));
 		$this->history[] = new History($name, $parameters, $wasCalled + 1);
+=======
+	function call(string $name, ...$parameters) {
+		$this->recordHistory($name, $parameters);
+
+<<<<<<< HEAD
+		if ($parameters[0] === 'essif-lab_insert_hook') {
+			$parameters[1](['slug' => 'title']);
+		}
+
+		if ($parameters[0] === 'essif-lab_delete_hook') {
+			$parameters[1](['slug' => 'title']);
+		}
+>>>>>>> 0b4d340... Added api for delete and insert hook
+=======
+		$this->handleSubPluginApi($name ,$parameters);
+>>>>>>> 6a9c1f7... Fixed broken tests after refactoring
 
 		if (array_key_exists($name, $this->callbackTriggeringFunctions)) {
 			$callback = $this->callbackTriggeringFunctions[$name];
@@ -44,14 +65,16 @@ class Utility extends BaseUtility {
 		return null;
 	}
 
-	/**
-	 * @param string $funcName
-	 * @return History[]
-	 */
-	function getHistoryByFuncName(string $funcName): array {
-		return array_slice(array_filter($this->history, function (History $history) use ($funcName) {
-			return $history->getFuncName() === $funcName;
-		}), 0);
+	static function handleSubPluginApi($name, array $params) {
+		if ($name === 'add_action') {
+			if ($params[0] === 'essif-lab_insert_hook') {
+				$params[1](['slug' => 'title']);
+			}
+
+			if ($params[0] === 'essif-lab_delete_hook') {
+				$params[1](['slug' => 'title']);
+			}
+		}
 	}
 
 	static function addHook(string $hook, callable $callback, int $priority = 10, int $accepted_args = 1): void {
@@ -63,9 +86,19 @@ class Utility extends BaseUtility {
 		$callback();
 	}
 
+<<<<<<< HEAD
     static function getModel(int $id): Model {
         return self::createModelWithId($id);
     }
+=======
+	static function getModel(int $id): Model {
+		return new Model([
+			Constants::TYPE_INSTANCE_IDENTIFIER_ATTR => $id,
+			Constants::TYPE_INSTANCE_TITLE_ATTR => 'hello',
+			Constants::TYPE_INSTANCE_DESCRIPTION_ATTR => 'world',
+		]);
+	}
+>>>>>>> 6a9c1f7... Fixed broken tests after refactoring
 
 	static function getCurrentModel(): Model {
 		return self::createModelWithId(1);
