6be4f10ebbc1a86f2f019d8c306c7f381dc4b061
Luuk van Houdt <luuk@vhoudt.nl>
2020-07-02 13:27:03 +0200

    added saving of input values before redirect and loading of values on load

--- plugins/essif-lab_contactform7/src/js/script.js
+++ plugins/essif-lab_contactform7/src/js/script.js
@@ -1,11 +1,18 @@
 (function ($) {
     $(window).load(function () {
+        let input
+        Object.keys(sessionStorage).map(function (slug) {
+            input = $("." + slug + " input");
+            let value = JSON.parse(sessionStorage.getItem(slug));
+            input.val(value.name);
+            if (value.immutable === true) input.prop("disabled", value.immutable);
+        });
+
         const urlParams = new URLSearchParams(window.location.search);
 
         const immutable = urlParams.get("immutable") === 'true';
         urlParams.delete("immutable")
 
-        let input;
         for (const entry of urlParams.entries()) {
             input = $("." + entry[0] + " input");
             input.val(entry[1]);
@@ -26,6 +33,16 @@
                 url: '../wp-json/jwt/v1/callbackurl=' + callbackUrl + '&inputslug=' + name,
                 success: function (data) {
                     if (data != null) {
+                        $(".wpcf7-form-control").not('.wpcf7-submit, .essif-lab').each(function () {
+                            if ($(this).val()) {
+                                sessionStorage.setItem($(this).attr('name'), JSON.stringify({
+                                    name: $(this).val(),
+                                    immutable: $(this).prop('disabled')
+                                }));
+                            } else {
+                                sessionStorage.removeItem($(this).attr('name'));
+                            }
+                        });
                         window.location.href = 'https://service.ssi-lab.sensorlab.tno.nl/verify/' + data;
                     }
                 },
